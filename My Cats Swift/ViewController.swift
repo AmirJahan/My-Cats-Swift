4833890df7a466ac6d8dd345e8d882e997cd8e63
Amir J <amir.jahan@gmail.com>
2017-10-30 09:08:24 -0700

    Initial Commit

---
+++ My Cats Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Cats Swift
+//
+//  Created by Amir on 10/30/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
